c0e62fa244b03c3317399626e0d4b16e67ece451
wonderlandpark <wonderbotsupport@naver.com>
2021-03-29 19:27:21 +0900

    feat: added icons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { FaEnvelope, FaGithub } from 'react-icons/fa'
 import Container from './Container'
 
 const Hero:FC = () => {
@@ -6,6 +7,14 @@ const Hero:FC = () => {
 		<div className='text-center'>
 			<h1 className='text-7xl font-black text-transparent bg-clip-text bg-gradient-to-r from-sea-blue via-hot-pink to-chick-yellow'>박준서</h1>
 			<p className='text-xl mt-2'>평범한 학생 풀스택 개발자입니다.</p>
+			<div className='flex justify-center text-xl mt-2'>
+				<a className='hover:text-hot-pink px-1' href='mailto:me@wonder.im'>
+					<FaEnvelope />
+				</a>
+				<a className='hover:text-chick-yellow px-1' href='https://github.com/wonderlandpark' target='_blank' rel='noreferrer'>
+					<FaGithub />
+				</a>
+			</div>
 		</div>
 	</Container>
 }
