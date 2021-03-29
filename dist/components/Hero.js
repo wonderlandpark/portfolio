b9557b8f17e6418c22f6f4b94610cf0e9962b30c
wonderlandpark <wonderlandpark@outlook.kr>
2021-03-29 10:28:17 +0000

    Deploying to gh-pages from @ wonderlandpark/portfolio@c0e62fa244b03c3317399626e0d4b16e67ece451 🚀

--- dist/components/Hero.js
+++ dist/components/Hero.js
@@ -1,4 +1,5 @@
 import React from "../../_snowpack/pkg/react.js";
+import {FaEnvelope, FaGithub} from "../../_snowpack/pkg/react-icons/fa.js";
 import Container from "./Container.js";
 const Hero = () => {
   return /* @__PURE__ */ React.createElement(Container, {
@@ -11,6 +12,16 @@ const Hero = () => {
     className: "text-7xl font-black text-transparent bg-clip-text bg-gradient-to-r from-sea-blue via-hot-pink to-chick-yellow"
   }, "박준서"), /* @__PURE__ */ React.createElement("p", {
     className: "text-xl mt-2"
-  }, "평범한 학생 풀스택 개발자입니다.")));
+  }, "평범한 학생 풀스택 개발자입니다."), /* @__PURE__ */ React.createElement("div", {
+    className: "flex justify-center text-xl mt-2"
+  }, /* @__PURE__ */ React.createElement("a", {
+    className: "hover:text-hot-pink px-1",
+    href: "mailto:me@wonder.im"
+  }, /* @__PURE__ */ React.createElement(FaEnvelope, null)), /* @__PURE__ */ React.createElement("a", {
+    className: "hover:text-chick-yellow px-1",
+    href: "https://github.com/wonderlandpark",
+    target: "_blank",
+    rel: "noreferrer"
+  }, /* @__PURE__ */ React.createElement(FaGithub, null)))));
 };
 export default Hero;
