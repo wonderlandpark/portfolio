03370f7ff6a0f72335aad1936f4418172c8267e3
wonderlandpark <wonderbotsupport@naver.com>
2021-04-03 11:50:04 +0900

    chore: changed hero font style

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,8 +5,8 @@ import Container from './Container'
 const Hero:FC = () => {
 	return <Container className='flex items-center justify-center' id='hero' ignorePadding>
 		<div className='text-center'>
-			<h1 className='text-7xl font-black text-transparent bg-clip-text bg-gradient-to-r from-sea-blue via-hot-pink to-chick-yellow'>박준서</h1>
-			<p className='text-xl mt-2'>평범한 학생 풀스택 개발자입니다.</p>
+			<h1 className='text-6xl font-black text-transparent bg-clip-text bg-gradient-to-r from-sea-blue via-hot-pink to-chick-yellow'>박준서</h1>
+			<p className='text-lg font-medium mt-2'>평범한 학생 풀스택 개발자입니다.</p>
 			<div className='flex justify-center text-xl mt-2'>
 				<a className='hover:text-hot-pink px-1' href='mailto:me@wonder.im'>
 					<FaEnvelope />
