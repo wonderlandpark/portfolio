b24c974e8281bb5ba4ce9329570c1f64d5376c90
Junseo Park <wonderlandpark@outlook.kr>
2022-05-19 22:06:10 +0900

    chore: removed twitter

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,7 +9,6 @@ const Contact:FC = () => {
 			<li>Email: <Link href='mailto:me@wonder.im'>me@wonder.im</Link> <Link href='wonderlandpark@callisto.team'>wonderlandpark@callisto.team</Link></li>
 			<li>Github: <Link href='https://github.com/wonderlandpark'>@wonderlandpark</Link></li>
 			<li>Discord: @wonderlandpark#9999</li>
-			<li>Twitter: <Link href='https://twitter.com/wonderlandpark0'>@wonderlandpark0</Link></li>
 			<li>Clubhouse: @wonderlandpark</li>
 		</ul>
 		<h1 className='text-5xl font-bold pt-10'><span role='img' aria-label='globe'>🌐</span> 소셜</h1>
